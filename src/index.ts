b606848a8fc8e70b94b7d5f77ec35c02c2f947f4
Alberto Rico <alrico88@gmail.com>
2023-01-09 17:36:14 +0100

    build: updated workflows

--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,6 @@ export function compressDateRange(
     formattedEnd,
     fullYearsBetween
   );
-  console.log(fullMonthsBetween);
   const daysBetween = getDaysInRange(
     formattedStart,
     formattedEnd,
